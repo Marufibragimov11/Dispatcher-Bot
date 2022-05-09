c3a4e262540990da195311b3337a54e83fc941ea
ibrag <immarufibragimov@yandex.com>
2022-05-09 17:46:51 +0500

    added some cities

--- cities.py
+++ cities.py
@@ -1,4 +1,9 @@
 fergana_name = ["фаргона", "фергана", "Farg'ona", "fargona"]
 andijon_name = ["Andijon", "Anjan", "Андижон", "Андижан"]
-tashkent_name = ["Tashkent", "Toshkent", "Tawkent", "Toshkend", "Tashkend", "Tashkendan"]
-
+tashkent_name = ["Tashkent", "Toshkent", "Tawkent", "Toshkend", "Tashkend", "Tashkendan", "Тошкент", "Ташкент"]
+namangan_name = ["Namangan", "Наманган"]
+sirdarya_name = ["Sirdaryo", "Сирдарйо", "Сардоба", "Сирдарё", "Сырдарья", "Guliston"]
+jizzakh_name = ["Jizzax", "Jizzah", "Жиззах", "Джиззах", "Zomin"]
+samarkand_name = ["Samarqand", "Samarqant", "Urgut", "Самарканд", "Самаркант"]
+bukhara_name = ["Buxoro", "Бухоро", "Бухара"]
+navoi_name = ["Navoi", "Navoiy", "Навои", "Наваи", "Навоий"]
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from telegram import Bot
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
-from cities import fergana_name, andijon_name, tashkent_name
+
+# Local files --------------------------------------------------------------------------------------------------
+from cities import (fergana_name, andijon_name, tashkent_name, namangan_name, sirdarya_name, jizzakh_name,
+                    samarkand_name, bukhara_name, navoi_name)
 from Keyboards import main_buttons
 
 STATE_CALENDAR = 1
@@ -9,6 +12,13 @@ bot = Bot
 t_list = set()
 f_list = set()
 a_list = set()
+n_list = set()
+sirdarya_users = set()
+jizzakh_users = set()
+samarkand_users = set()
+bukhara_users = set()
+navoi_users = set()
+kashkadarya_users = set()
 
 
 def start(update, context):
@@ -40,9 +50,6 @@ def Tashkent_loads(update, context):
                                                        from_chat_id="@asdfghsgda",
                                                        message_id=update.message.message_id)
         print(update.message['text'])
-        # for tosh in toshkent:
-        #     re_text = re.findall(tosh, update.message.message_id)
-        #     print(re)
 
 
 def Andijan_loads(update, context):
@@ -82,38 +89,101 @@ def Ferghana_loads(update, context):
 
 
 def Namangan_loads(update, context):
-    update.message.reply_text(
-        'Endi men sizga Namangan dagi yuklarni yuborib turaman 😊')
+    if update.message.chat.type == "private":
+        update.message.reply_text(
+            'Endi men sizga Namangan dagi yuklarni yuborib turaman 😊')
+        n_list.add(update.message.from_user.id)
+    elif update.message.chat.type == "supergroup":
+        for nst in n_list:
+            for namangan in namangan_name:
+                if namangan in update.message.text:
+                    update.message.bot.forward_message(chat_id=nst,
+                                                       from_chat_id="@asdfghsgda",
+                                                       message_id=update.message.message_id)
 
 
 def Sirdarya_loads(update, context):
-    update.message.reply_text(
-        'Endi men sizga Sirdaryo dagi yuklarni yuborib turaman 😊')
+    if update.message.chat.type == "private":
+        update.message.reply_text(
+            'Endi men sizga Sirdaryo dagi yuklarni yuborib turaman 😊')
+        sirdarya_users.add(update.message.from_user.id)
+    elif update.message.chat.type == "supergroup":
+        for sir_lst in sirdarya_users:
+            for sirdarya in sirdarya_name:
+                if sirdarya in update.message.text:
+                    update.message.bot.forward_message(chat_id=sir_lst,
+                                                       from_chat_id="@asdfghsgda",
+                                                       message_id=update.message.message_id)
 
 
 def Djizzak_loads(update, context):
-    update.message.reply_text(
-        'Endi men sizga Jizzax dagi yuklarni yuborib turaman 😊')
+    if update.message.chat.type == "private":
+        update.message.reply_text(
+            'Endi men sizga Jizzax dagi yuklarni yuborib turaman 😊')
+        jizzakh_users.add(update.message.from_user.id)
+    elif update.message.chat.type == "supergroup":
+        for jiz_list in jizzakh_users:
+            for jizzakh in jizzakh_name:
+                if jizzakh in update.message.text:
+                    update.message.bot.forward_message(chat_id=jiz_list,
+                                                       from_chat_id="@asdfghsgda",
+                                                       message_id=update.message.message_id)
 
 
-def Samarkan_loads(update, context):
-    update.message.reply_text(
-        'Endi men sizga Samarkand dagi yuklarni yuborib turaman 😊')
+def Samarkand_loads(update, context):
+    if update.message.chat.type == "private":
+        update.message.reply_text(
+            'Endi men sizga Samarqand dagi yuklarni yuborib turaman 😊')
+        samarkand_users.add(update.message.from_user.id)
+    elif update.message.chat.type == "supergroup":
+        for sam_list in samarkand_users:
+            for samarkand in samarkand_name:
+                if samarkand in update.message.text:
+                    update.message.bot.forward_message(chat_id=sam_list,
+                                                       from_chat_id="@asdfghsgda",
+                                                       message_id=update.message.message_id)
 
 
 def Bukhara_loads(update, context):
-    update.message.reply_text(
-        'Endi men sizga Buxoro dagi yuklarni yuborib turaman 😊')
+    if update.message.chat.type == "private":
+        update.message.reply_text(
+            'Endi men sizga Buxoro dagi yuklarni yuborib turaman 😊')
+        bukhara_users.add(update.message.from_user.id)
+    elif update.message.chat.type == "supergroup":
+        for bukhara_list in bukhara_users:
+            for bukhara in bukhara_name:
+                if bukhara in update.message.text:
+                    update.message.bot.forward_message(chat_id=bukhara_list,
+                                                       from_chat_id="@asdfghsgda",
+                                                       message_id=update.message.message_id)
 
 
 def Navoi_loads(update, context):
-    update.message.reply_text(
-        'Endi men sizga Navoi dagi yuklarni yuborib turaman 😊')
+    if update.message.chat.type == "private":
+        update.message.reply_text(
+            'Endi men sizga Navoi dagi yuklarni yuborib turaman 😊')
+        navoi_users.add(update.message.from_user.id)
+    elif update.message.chat.type == "supergroup":
+        for navoi_list in navoi_users:
+            for navoi in navoi_name:
+                if navoi in update.message.text:
+                    update.message.bot.forward_message(chat_id=navoi_list,
+                                                       from_chat_id="@asdfghsgda",
+                                                       message_id=update.message.message_id)
 
 
 def Kashkadarya_loads(update, context):
-    update.message.reply_text(
-        'Endi men sizga Qashqadaryo dagi yuklarni yuborib turaman 😊')
+    if update.message.chat.type == "private":
+        update.message.reply_text(
+            'Endi men sizga Navoi dagi yuklarni yuborib turaman 😊')
+        navoi_users.add(update.message.from_user.id)
+    elif update.message.chat.type == "supergroup":
+        for navoi_list in navoi_users:
+            for navoi in navoi_name:
+                if navoi in update.message.text:
+                    update.message.bot.forward_message(chat_id=navoi_list,
+                                                       from_chat_id="@asdfghsgda",
+                                                       message_id=update.message.message_id)
 
 
 def Surkhandarya_loads(update, context):
@@ -143,6 +213,14 @@ def main():
         dispatcher.add_handler(MessageHandler(Filters.regex('(' + anjan_name + ')'), Andijan_loads))
     for ferg_name in fergana_name:
         dispatcher.add_handler(MessageHandler(Filters.regex('(' + ferg_name + ')'), Ferghana_loads))
+    for naman_name in namangan_name:
+        dispatcher.add_handler(MessageHandler(Filters.regex('(' + naman_name + ')'), Namangan_loads))
+    for sirdar_name in sirdarya_name:
+        dispatcher.add_handler(MessageHandler(Filters.regex('(' + sirdar_name + ')'), Sirdarya_loads))
+    for jizza_name in jizzakh_name:
+        dispatcher.add_handler(MessageHandler(Filters.regex('(' + jizza_name + ')'), Djizzak_loads))
+    for sam_name in samarkand_name:
+        dispatcher.add_handler(MessageHandler(Filters.regex('(' + sam_name + ')'), Samarkand_loads))
 
     #     conv_handler = ConversationHandler(
     #         entry_points=[CommandHandler('start', start),
@@ -151,18 +229,6 @@ def main():
     #         states={
     #             STATE_CALENDAR: [
     #                 MessageHandler(Filters.regex('^(' + Tashkent + ')$'), Tashkent_loads),
-    #                 MessageHandler(Filters.regex('^(' + Andijan + ')$'), Andijan_loads),
-    #                 MessageHandler(Filters.regex('^(' + Ferghana + ')$'), Ferghana_loads),
-    #                 MessageHandler(Filters.regex('^(' + Namangan + ')$'), Namangan_loads),
-    #                 MessageHandler(Filters.regex('^(' + Sirdarya + ')$'), Sirdarya_loads),
-    #                 MessageHandler(Filters.regex('^(' + Djizzak + ')$'), Djizzak_loads),
-    #                 MessageHandler(Filters.regex('^(' + Samarkand + ')$'), Samarkan_loads),
-    #                 MessageHandler(Filters.regex('^(' + Bukhara + ')$'), Bukhara_loads),
-    #                 MessageHandler(Filters.regex('^(' + Navoi + ')$'), Navoi_loads),
-    #                 MessageHandler(Filters.regex('^(' + Kashkadarya + ')$'), Kashkadarya_loads),
-    #                 MessageHandler(Filters.regex('^(' + Surhandarya + ')$'), Surkhandarya_loads),
-    #                 MessageHandler(Filters.regex('^(' + Horezm + ')$'), Xorezm_loads),
-    #                 MessageHandler(Filters.regex('^(' + Karakalpakistan + ')$'), Karakalpak_loads)
     #             ],
     #         },
     #         fallbacks=[CommandHandler('start', start)]
@@ -176,5 +242,3 @@ def main():
 
 
 main()
-
-# Bu proyekt Burxon tomonidan yangilandi
